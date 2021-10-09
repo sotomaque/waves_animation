03e2c117bb638b21420306e04ecd8bc6b666e715
sotomaque <sotomaque@gmail.com>
2021-10-09 16:13:24 -0700

    Initial Commit

---
+++ waves_animation/waves_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  waves_animationApp.swift
+//  waves_animation
+//
+//  Created by Enrique Sotomayor on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct waves_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
